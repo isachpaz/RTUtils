855477f9261903eec96029289c58dfbe319d01bf
Dr. Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2020-06-21 21:03:56 +0200

    CompressedData renamed

--- RTUtils/RTUtils.Misc/CompressedData.cs
+++ RTUtils/RTUtils.Misc/CompressedData.cs
@@ -2,13 +2,13 @@
 
 namespace RTUtils.Misc
 {
-    public class CompressedResult
+    public class CompressedData
     {
         public uint [] Data { get; }
         public double Scale { get; }
         public double Offset { get; }
 
-        public CompressedResult(UInt32[] data, double scale, double offset)
+        public CompressedData(UInt32[] data, double scale, double offset)
         {
             Data = data;
             Scale = scale;
