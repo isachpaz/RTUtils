d29b8ceffa8151b4c5747a2af1370b62aa45340e
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2020-07-01 23:50:47 +0200

    DicomUniqueID added

---
+++ RTUtils/RTUtils.Misc/DicomUniqueID.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace RTUtils.Misc
+{
+    public class DicomUniqueID
+    {
+        public static string RootUID { get; set; }
+
+        public static string Generate(string name = "")
+        {
+            if (string.IsNullOrEmpty(RootUID))
+            {
+                RootUID = "1.2.826.0.1.3680043.2.1343.1";
+            }
+
+            var uid = $"{RootUID}.{DateTime.UtcNow}.{DateTime.UtcNow.Millisecond*97}"
+                .Replace("/",".")
+                .Replace(":",".")
+                .Replace(" ",".");
+
+            return uid;
+        }
+
+
+    }
+}
---
+++ RTUtils/RTUtils.MiscTest/DicomUniqueIDTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using RTUtils.Misc;
+
+namespace RTUtils.MiscTests
+{
+    [TestFixture]
+    public class DicomUniqueIDTests
+    {
+        [Test]
+        public void Generate_DICOMUID_Test()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var uid = DicomUniqueID.Generate();
+                Debug.WriteLine(uid);
+
+                //TODO: Check for spaces, and special chars and non numeric
+            }
+            
+        }
+    }
+}
